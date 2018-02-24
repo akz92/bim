d53c46e56f657cd3d38aa34992955865c1403258
akz92 <aride.moulin@gmail.com>
2018-02-24 11:12:42 -0300

    [fix] Reload button for index 0

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@ v<template>
       </svg>
     </i>
 
-    <i id="navbar-reload" class="nav-icons" title="Reload page" v-bind:class="{ disabled: !activeIndex }" @click="toggleReload">
+    <i id="navbar-reload" class="nav-icons" title="Reload page" v-bind:class="{ disabled: !hasActiveIndex }" @click="toggleReload">
       <svg v-if="loading" height="100%" viewBox="0 0 24 24">
         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
         <path d="M0 0h24v24H0z" fill="none"/>
@@ -58,7 +58,8 @@ export default {
       'loading',
       'canGoBack',
       'canGoForward'
-    ])
+    ]),
+    hasActiveIndex: function () { return typeof this.activeIndex === 'number' }
   },
   methods: {
     ...mapActions([
