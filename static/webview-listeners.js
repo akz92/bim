66206f3c4f75a1cb947a30fbbd72c42328a5dadf
akz92 <aride.moulin@gmail.com>
2018-03-09 09:45:40 -0300

    Undo buggy listeners init

--- static/webview-listeners.js
+++ static/webview-listeners.js
@@ -1,41 +1,38 @@
 const {ipcRenderer} = require('electron')
 
-function registerListeners() {
-  // Mode changes
-  window.addEventListener('focus', function () {
-    ipcRenderer.sendToHost('window:focus')
-  })
+// Mode changes
+window.addEventListener('focus', function () {
+  console.log('window:focus');
+  ipcRenderer.sendToHost('window:focus')
+})
 
-  // Fix focus when iframe is the active element
-  ipcRenderer.on('document:activeelement:blur', () => {
-    if (document.activeElement) {
-      document.activeElement.blur()
-    }
-  })
+// Fix focus when iframe is the active element
+ipcRenderer.on('document:activeelement:blur', () => {
+  if (document.activeElement) {
+    document.activeElement.blur()
+  }
+})
 
-  document.addEventListener('hint:close', function () {
-    ipcRenderer.sendToHost('hint:close')
-  })
+document.addEventListener('hint:close', function () {
+  ipcRenderer.sendToHost('hint:close')
+})
 
-  document.addEventListener('hint:yank', function (ev) {
-    ipcRenderer.sendToHost('hint:yank', ev.detail.url)
-  })
+document.addEventListener('hint:yank', function (ev) {
+  ipcRenderer.sendToHost('hint:yank', ev.detail.url)
+})
 
-  document.addEventListener('hint:focusinput', function () {
-    ipcRenderer.sendToHost('hint:focusinput')
-  })
+document.addEventListener('hint:focusinput', function () {
+  ipcRenderer.sendToHost('hint:focusinput')
+})
 
-  // Navigation
-  ipcRenderer.on('scroll:top', pageScroll.goToPageTop)
-  ipcRenderer.on('scroll:bottom', pageScroll.goToPageBottom)
-  ipcRenderer.on('scroll:down:halfpage', pageScroll.scrollDownHalfPage)
-  ipcRenderer.on('scroll:up:halfpage', pageScroll.scrollUpHalfPage)
+// Navigation
+ipcRenderer.on('scroll:top', pageScroll.goToPageTop)
+ipcRenderer.on('scroll:bottom', pageScroll.goToPageBottom)
+ipcRenderer.on('scroll:down:halfpage', pageScroll.scrollDownHalfPage)
+ipcRenderer.on('scroll:up:halfpage', pageScroll.scrollUpHalfPage)
 
-  // Hints
-  ipcRenderer.on('hints:show', () => linkHints.activateLinkHintsMode())
-  ipcRenderer.on('hints:show-tab', linkHints.activateLinkHintsModeToOpenInNewTab)
-  ipcRenderer.on('hints:show-yank', linkHints.activeLinkHinstModeYank)
-  ipcRenderer.on('hints:hide', linkHints.deactivateLinkHintsMode)
-}
-
-document.onready = registerListeners
+// Hints
+ipcRenderer.on('hints:show', () => linkHints.activateLinkHintsMode())
+ipcRenderer.on('hints:show-tab', linkHints.activateLinkHintsModeToOpenInNewTab)
+ipcRenderer.on('hints:show-yank', linkHints.activeLinkHinstModeYank)
+ipcRenderer.on('hints:hide', linkHints.deactivateLinkHintsMode)
